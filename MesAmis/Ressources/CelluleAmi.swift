d50721c31492c59f8cec980e7fa8eb229448b0da
Alain <alain@LD140-PR-06284.local>
2017-10-27 10:53:58 -0400

    version 1

---
+++ MesAmis/Ressources/CelluleAmi.swift
@@ -0,0 +1,17 @@
+//
+//  CelluleAmi.swift
+//  MesAmis
+//
+//  Created by Alain on 17-10-27.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+import UIKit
+
+class CelluleAmi: UICollectionViewCell {
+    
+    @IBOutlet weak var amiNom: UILabel!
+    @IBOutlet weak var amiDette: UILabel!
+    @IBOutlet weak var amiImage: UIImageView!
+    
+}
--- MesAmis/ViewController.swift
+++ MesAmis/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource {
+  
+    
+  var mesAmis:Array<Dictionary<String,String>> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        // 1 - Charger les données
+        let pathFichierPlist = Bundle.main.path(forResource: "liste des amis", ofType: "plist")!
+        mesAmis = NSArray(contentsOfFile: pathFichierPlist) as! Array
+        print(mesAmis)
+    } // viewDidLoad
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+    // 2 - programmer UICollectionViewDataSource
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mesAmis.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let indice = indexPath.row
+        
+        print("cellule no: \(indice)")
+        let nom = mesAmis[indice]["nom"] ?? "N/D"
+        let dette = mesAmis[indice]["dette"] ?? "N/D"
+        let image = mesAmis[indice]["photo"] ?? "nd.jpg"
+        
+        let nomModele =  Float(dette)! <= 10 ? "vert":"rouge"
+        
+        let cellule = collectionView.dequeueReusableCell(withReuseIdentifier: nomModele, for: indexPath) as! CelluleAmi
+        
+        
+       
+        
+        cellule.amiNom?.text = nom
+        cellule.amiDette?.text = dette
+        cellule.amiImage?.image = UIImage(named:image)
+        return cellule
+    }
 
 }
 
